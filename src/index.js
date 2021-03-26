74940a2b1d16c94941c62afea13acc95001a05db
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-03-25 22:37:09 -0500

    lab02 paso7

--- src/index.js
+++ src/index.js
@@ -26,26 +26,29 @@ function Total({parts}){
   )
 }
 const App = () => {
-  const course = 'Desarrollo de Aplicaciones Web Avanzada'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Desarrollo de Aplicaciones Web Avanzada',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
